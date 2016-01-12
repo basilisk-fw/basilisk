b3e1ff5407ca5182caf3574a7b4c44e567e79966
aalmiray <aalmiray@gmail.com>
2016-01-12 20:38:44 +0100

    Bum version to next snapshot

--- subprojects/gradle-basilisk-plugin/src/main/groovy/org/kordamp/basilisk/gradle/BasiliskExtension.groovy
+++ subprojects/gradle-basilisk-plugin/src/main/groovy/org/kordamp/basilisk/gradle/BasiliskExtension.groovy
@@ -21,7 +21,7 @@ import org.gradle.api.Project
  * @author Andres Almiray
  */
 class BasiliskExtension {
-    String version = '0.1.0'
+    String version = '0.2.0-SNAPSHOT'
 
     boolean disableDependencyResolution
 
